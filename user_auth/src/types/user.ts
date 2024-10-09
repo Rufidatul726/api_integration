9ceb5041bde2708161d54fb13d2bee24a11c963e
Rufidatul726 <bsse1226@iit.du.ac.bd>
2024-10-10 03:21:36 +0600

    user-auth

---
+++ user_auth/src/types/user.ts
@@ -0,0 +1,8 @@
+export type User = {
+    id? : string | null;
+    name? : string | null;
+    email? : string | null;
+    password? : string | null;
+    confirm_password? : string | null;
+    options? : object | {} ;
+}
